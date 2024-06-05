4b11e4fba73ff35ca2f7ed87859ec8f928929f26
Anfo <longjiahui@hotmail.com>
2024-06-05 11:15:22 +0800

    feat: 1

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,7 @@
 #include <iostream>
+#include <stdlib.h>
 #include <GLFW/glfw3.h>
-#include "vulkan.hpp"
-
-void TitleFps(GLFWwindow *pWindow)
-{
-    static double time0 = glfwGetTime();
-    static double time1;
-    static double dt;
-    static int dframe = -1;
-    static std::stringstream info;
-    time1 = glfwGetTime();
-    dframe++;
-    if ((dt = time1 - time0) >= 1)
-    {
-        info.precision(1);
-        info << "anfo window" << "    " << std::fixed << dframe / dt << " FPS";
-        glfwSetWindowTitle(pWindow, info.str().c_str());
-        info.str(""); // 别忘了在设置完窗口标题后清空所用的stringstream
-        time0 = time1;
-        dframe = 0;
-    }
-}
+#include "default.hpp"
 
 int main(void)
 {
@@ -28,33 +9,27 @@ int main(void)
 
     /* Initialize the library */
     if (!glfwInit())
-        return -1;
+        return EXIT_FAILURE;
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    window = glfwCreateWindow(640, 480, "Hello World", nullptr, nullptr);
     if (!window)
     {
         glfwTerminate();
-        return -1;
+        return EXIT_FAILURE;
     }
 
     /* Make the window's context current */
-    glfwMakeContextCurrent(window);
+    // glfwMakeContextCurrent(window);
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
-        /* Render here */
-        // glClear(GL_COLOR_BUFFER_BIT);
-
-        TitleFps(window);
-        /* Swap front and back buffers */
-        // glfwSwapBuffers(window);
-
+        glfwSwapBuffers(window);
         /* Poll for and process events */
         glfwPollEvents();
     }
 
     glfwTerminate();
-    return 0;
+    return EXIT_SUCCESS;
 }
